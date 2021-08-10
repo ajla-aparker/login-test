a18bd009ae40a50bd9e54e08673a5d2705549c1a
Adam Parker <adam.parker@ks.gov>
2021-08-10 15:07:12 -0500

    style changes, remove build from repo

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Fieldset, Form, Label, TextInput, ValidationChecklist, ValidationItem } from '@trussworks/react-uswds';
+import { Alert, Button, Fieldset, Form, Label, TextInput, ValidationItem } from '@trussworks/react-uswds';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
@@ -24,59 +24,56 @@ const LoginForm = () => {
   });
   
   return (
-    <Form onSubmit={handleSubmit(onSubmit)} large>
-      <Fieldset legend="Sign In" legendStyle="large">
-        <span>
-          or <a href="#">create an account</a>
-        </span>
-        {errors.email || errors.password ? <Alert type="info" validation heading="Form Requirements">
-          { errors.email ? <ValidationItem id="uppercase" isValid={!errors.email}>
-            {errors.email?.message}
-          </ValidationItem> : undefined }
-          { errors.password ? <ValidationItem id="numerical" isValid={!errors.password}>
-            {errors.password?.message}
-          </ValidationItem> : undefined }
-        </Alert> : undefined}
-        <Label htmlFor="email">Email address</Label>
-        <TextInput
-          id="email"
-          name="email"
-          type="text"
-          autoCapitalize="off"
-          autoCorrect="off"
-          {...register('email')}
-        />
-        <Label htmlFor="password-sign-in">Password</Label>
-        <TextInput
-          id="password-sign-in"
-          name="password-sign-in"
-          type="password"
-          {...register('password')}
-        />
-        <p className="usa-form__note">
-          <a
-            title="Show password"
-            href="#"
-            className="usa-show-password"
-            aria-controls="password-sign-in"
-            onClick={() => {}}
-          >
-          </a>
-        </p>
+    <div style={{"margin-left": "2em"}}>
+      <Form onSubmit={handleSubmit(onSubmit)} large>
+        <Fieldset legend="Sign In" legendStyle="large">
+          <legend>
+            Access your account
+          </legend>
+          {errors.email || errors.password ? <Alert type="info" validation heading="Form Requirements">
+            { errors.email ? <ValidationItem id="uppercase" isValid={!errors.email}>
+              {errors.email?.message}
+            </ValidationItem> : undefined }
+            { errors.password ? <ValidationItem id="numerical" isValid={!errors.password}>
+              {errors.password?.message}
+            </ValidationItem> : undefined }
+          </Alert> : undefined}
+          <Label htmlFor="email">Email address</Label>
+          <TextInput
+            id="email"
+            name="email"
+            type="text"
+            autoCapitalize="off"
+            autoCorrect="off"
+            {...register('email')}
+          />
+          <Label htmlFor="password-sign-in">Password</Label>
+          <TextInput
+            id="password-sign-in"
+            name="password-sign-in"
+            type="password"
+            {...register('password')}
+          />
+          <p className="usa-form__note">
+            <a
+              title="Show password"
+              href="#"
+              className="usa-show-password"
+              aria-controls="password-sign-in"
+              onClick={() => {}}
+            >
+            </a>
+          </p>
 
-        <Button type="submit">Sign in</Button>
-        <p>
-          <a href="#" title="Forgot username">
-            Forgot username?
-          </a>
-        </p>
-        <p>
-          <a href="#" title="Forgot password">
-            Forgot password?
-          </a>
-        </p>
-      </Fieldset>
-    </Form>
+          <Button type="submit">Sign in</Button>
+          <p>
+            <a href="#" title="Forgot password">
+              Forgot password?
+            </a>
+          </p>
+        </Fieldset>
+      </Form>
+    </div>
   )
 }
 
